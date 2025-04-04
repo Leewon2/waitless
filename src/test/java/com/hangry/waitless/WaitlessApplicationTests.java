58b3aa8f04254c6e6357d71ae9f891c0e83c37fe
Leewon2 <thehee0407@naver.com>
2025-04-04 19:34:34 +0900

    first commit

---
+++ src/test/java/com/hangry/waitless/WaitlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangry.waitless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
