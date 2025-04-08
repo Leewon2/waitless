771931403c7257e7a5e127e8513ea01fca64a049
Leewon2 <thehee0407@naver.com>
2025-04-09 04:29:12 +0900

    fix : Menu entity 수정, .gitKeep 제거

--- restaurant-service/src/main/java/com/waitless/restaurant/menu/domain/entity/Menu.java
+++ restaurant-service/src/main/java/com/waitless/restaurant/menu/domain/entity/Menu.java
@@ -1,11 +1,11 @@
 package com.waitless.restaurant.menu.domain.entity;
 
 import com.waitless.common.entity.BaseTimeEntity;
+import com.waitless.restaurant.menu.application.dto.CreateMenuDto;
 import com.waitless.restaurant.menu.domain.entity.enums.MenuCategory;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.Filter;
 
 import java.util.UUID;
 
@@ -13,6 +13,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "p_menu")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Filter(name = "deletedFilter", condition = "(deleted_at IS NOT NULL) = :isDeleted")
 public class Menu extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -27,6 +28,21 @@ public class Menu extends BaseTimeEntity {
     @Column(nullable = false)
     private int amount;
 
+    @Column(nullable = false)
     private int price;
 
+    @Column(nullable = false)
+    private String name;
+
+    public static Menu of(CreateMenuDto createMenuDto){
+        return new Menu(createMenuDto.restaurantId(), createMenuDto.category(), createMenuDto.amount(), createMenuDto.price(), createMenuDto.name());
+    }
+
+    private Menu(UUID restaurantId, MenuCategory menuCategory, int amount, int price, String name){
+        this.restaurantId=restaurantId;
+        this.menuCategory=menuCategory;
+        this.amount=amount;
+        this.price=price;
+        this.name=name;
+    }
 }
