7dd38dba9d14055a47b3c34db7e786f5c3c8be33
Leewon2 <thehee0407@naver.com>
2025-04-08 12:49:02 +0900

    feat : Menu Entity 생성

---
+++ restaurant-service/src/main/java/com/waitless/restaurant/menu/domain/entity/Menu.java
@@ -0,0 +1,32 @@
+package com.waitless.restaurant.menu.domain.entity;
+
+import com.waitless.common.entity.BaseTimeEntity;
+import com.waitless.restaurant.menu.domain.entity.enums.MenuCategory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "p_menu")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Menu extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID restaurantId;
+
+    @Enumerated(EnumType.STRING)
+    private MenuCategory menuCategory;
+
+    @Column(nullable = false)
+    private int amount;
+
+    private int price;
+
+}
