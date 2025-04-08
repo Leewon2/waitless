4148e48041645fa3e9c7109b1c086e7c5cd29255
Leewon2 <thehee0407@naver.com>
2025-04-09 04:33:27 +0900

    feat : EnableJpaAudition 추가

--- restaurant-service/src/main/java/com/waitless/restaurant/RestaurantApplication.java
+++ restaurant-service/src/main/java/com/waitless/restaurant/RestaurantApplication.java
@@ -2,11 +2,10 @@ package com.waitless.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(scanBasePackages = {"com.waitless.restaurant", "common"},exclude={
-	DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaAuditing
 public class RestaurantApplication {
 
 	public static void main(String[] args) {
