11c52daf604d32bd9bd74830308ebcb2be765864
Allrace <jeonij23@gmail.com>
2025-04-10 19:35:23 +0900

    feat : 예약생성에 사용하는 요청, 응답 dto 생성 #17

---
+++ reservation-service/src/main/java/com/waitless/reservation/application/dto/ReservationCreateCommand.java
@@ -0,0 +1,21 @@
+package com.waitless.reservation.application.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public record ReservationCreateCommand(
+        UUID restaurantId,
+        List<MenuCommandDto> menus,
+        String restaurantName,
+        Integer peopleCount,
+        LocalDate reservationDate,
+        Long userId
+) {
+    public record MenuCommandDto(
+            UUID menuId,
+            String menuName,
+            Integer quantity,
+            Integer price
+    ) {}
+}
