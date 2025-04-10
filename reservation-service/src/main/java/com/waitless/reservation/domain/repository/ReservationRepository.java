701eaab58bb8e848c441741e1d0275176db9a0bf
Allrace <jeonij23@gmail.com>
2025-04-10 19:40:40 +0900

    feat : 레포지토리 생성 #17

---
+++ reservation-service/src/main/java/com/waitless/reservation/domain/repository/ReservationRepository.java
@@ -0,0 +1,12 @@
+package com.waitless.reservation.domain.repository;
+
+import com.waitless.reservation.domain.entity.Reservation;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ReservationRepository {
+    void save(Reservation reservation);
+
+    Optional<Reservation> findById(UUID id);
+}
---
+++ reservation-service/src/main/java/com/waitless/reservation/infrastructure/adaptor/repository/ReservationRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.waitless.reservation.infrastructure.adaptor.repository;
+
+import com.waitless.reservation.domain.entity.Reservation;
+import com.waitless.reservation.domain.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class ReservationRepositoryImpl implements ReservationRepository {
+
+    private final ReservationJpaRepository reservationJpaRepository;
+
+    @Override
+    public void save(Reservation reservation) {
+        reservationJpaRepository.save(reservation);
+    }
+
+    @Override
+    public Optional<Reservation> findById(UUID id) {
+        return reservationJpaRepository.findById(id);
+    }
+}
