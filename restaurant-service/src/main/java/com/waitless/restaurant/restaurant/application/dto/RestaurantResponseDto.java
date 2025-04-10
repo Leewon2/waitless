115ac3d178aa1866ed4ca5556efa244d65c16639
juhee99 <eee6767@naver.com>
2025-04-10 12:49:33 +0900

    feat: ResponseDto 명 변경
    - CreateRestaurantResponseDto -> RestaurantResponseDto
    
    #37

---
+++ restaurant-service/src/main/java/com/waitless/restaurant/restaurant/application/dto/RestaurantResponseDto.java
@@ -0,0 +1,17 @@
+package com.waitless.restaurant.restaurant.application.dto;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+public record RestaurantResponseDto(UUID id,
+                                    String name,
+                                    Long ownerId,
+                                    String phone,
+                                    String categoryName,
+                                    Double latitude,
+                                    Double longitude,
+                                    LocalTime openingTime,
+                                    LocalTime closingTime
+) {
+
+}
