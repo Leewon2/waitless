9d5672107f3b1ad86dd72de706d1587b0e9f100a
Allrace <jeonij23@gmail.com>
2025-04-10 19:30:08 +0900

    feat : ErrorCode 상속받는 ReservationErrorCode 추가 #17

---
+++ reservation-service/src/main/java/com/waitless/reservation/exception/exception/ReservationErrorCode.java
@@ -0,0 +1,30 @@
+package com.waitless.reservation.exception.exception;
+
+import com.waitless.common.exception.code.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public enum ReservationErrorCode implements ErrorCode {
+
+    RESERVATION_STOCK_ERROR("RES_001", "재고가 부족합니다."),
+    RESERVATION_MENU_NOTFOUND_ERROR("RES_002", "존재하지 않는 메뉴입니다."),
+    RESERVATION_RESTAURANT_NOT_FOUND("RES_003", "존재하지 않는 식당입니다."),
+    UNKNOWN_LUA_RESULT("RES_999", "알 수 없는 Redis Lua 실행 결과입니다.");
+    private final String code;
+    private final String message;
+
+    ReservationErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
