3b9b2fefb6bebd2d0d9d20e4e1fe1aad359890d3
2sminn <sungmingod13@gmail.com>
2025-04-13 18:13:31 +0900

    [feat]: 리뷰 생성 로직 변경 #43

--- review-service/src/main/java/com/waitless/review/application/service/ReviewServiceImpl.java
+++ review-service/src/main/java/com/waitless/review/application/service/ReviewServiceImpl.java
@@ -1,12 +1,15 @@
 package com.waitless.review.application.service;
 
+import com.waitless.common.event.ReviewCreatedEvent;
 import com.waitless.review.application.dto.command.PostReviewCommand;
 import com.waitless.review.application.dto.result.PostReviewResult;
 import com.waitless.review.application.mapper.ReviewServiceMapper;
+import com.waitless.review.application.port.out.ReviewEventPublisher;
 import com.waitless.review.domain.entity.Review;
 import com.waitless.review.domain.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,12 +17,20 @@ public class ReviewServiceImpl implements ReviewService {
 
     private final ReviewRepository reviewRepository;
     private final ReviewServiceMapper reviewServiceMapper;
+    private final ReviewEventPublisher reviewEventPublisher;
 
     @Override
-
+    @Transactional
     public PostReviewResult createReview(PostReviewCommand command) {
         Review review = reviewServiceMapper.toEntity(command);
         Review saved = reviewRepository.save(review);
+
+        ReviewCreatedEvent event = ReviewCreatedEvent.of(
+                saved.getId(),
+                saved.getUserId(),
+                saved.getRestaurantId()
+        );
+        reviewEventPublisher.publishReviewCreated(event);
         return PostReviewResult.from(saved);
     }
 }
