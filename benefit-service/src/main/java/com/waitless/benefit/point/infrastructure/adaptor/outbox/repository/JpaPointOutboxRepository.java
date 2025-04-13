8dd81677f237904f07f8e13c663f8106a3a1caed
2sminn <sungmingod13@gmail.com>
2025-04-14 02:20:00 +0900

    [feat]:포인트 OutboxMessage 엔티티, repository 생성 #71

---
+++ benefit-service/src/main/java/com/waitless/benefit/point/infrastructure/adaptor/outbox/repository/JpaPointOutboxRepository.java
@@ -0,0 +1,11 @@
+package com.waitless.benefit.point.infrastructure.adaptor.outbox.repository;
+
+import com.waitless.benefit.point.infrastructure.adaptor.outbox.entity.PointOutboxMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface JpaPointOutboxRepository extends JpaRepository<PointOutboxMessage, UUID> {
+    List<PointOutboxMessage> findByStatus(PointOutboxMessage.OutboxStatus status);
+}
