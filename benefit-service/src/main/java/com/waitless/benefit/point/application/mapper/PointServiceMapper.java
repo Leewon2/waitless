7235561fc7a7dc3da559fef2e7e19b9f4b5edc85
2sminn <sungmingod13@gmail.com>
2025-04-14 02:24:34 +0900

    [feat]:포인트 mapper추가(Command->entity) #71

---
+++ benefit-service/src/main/java/com/waitless/benefit/point/application/mapper/PointServiceMapper.java
@@ -0,0 +1,19 @@
+package com.waitless.benefit.point.application.mapper;
+
+import com.waitless.benefit.point.application.dto.command.PostPointCommand;
+import com.waitless.benefit.point.domain.entity.Point;
+import com.waitless.benefit.point.domain.vo.PointAmount;
+import com.waitless.benefit.point.domain.vo.PointType;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PointServiceMapper {
+    default Point toEntity(PostPointCommand command) {
+        return Point.of(
+                command.userId(),
+                PointAmount.of(command.amount()),
+                PointType.of(command.pointType()),
+                command.description()
+        );
+    }
+}
