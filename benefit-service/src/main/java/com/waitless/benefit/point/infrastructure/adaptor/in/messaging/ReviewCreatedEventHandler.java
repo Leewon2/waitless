daaeb2709ea898c70ac035410a06a20c0a6cf981
2sminn <sungmingod13@gmail.com>
2025-04-13 19:11:25 +0900

    [feat]: 리뷰생성이벤트핸들러 수정 #43

--- benefit-service/src/main/java/com/waitless/benefit/point/infrastructure/adaptor/in/messaging/ReviewCreatedEventHandler.java
+++ benefit-service/src/main/java/com/waitless/benefit/point/infrastructure/adaptor/in/messaging/ReviewCreatedEventHandler.java
@@ -1,8 +1,7 @@
 package com.waitless.benefit.point.infrastructure.adaptor.in.messaging;
 
-import com.waitless.benefit.point.domain.entity.Point;
-import com.waitless.benefit.point.domain.repository.PointRepository;
-import com.waitless.benefit.point.domain.vo.PointAmount;
+import com.waitless.benefit.point.application.dto.command.PostPointCommand;
+import com.waitless.benefit.point.application.service.PointService;
 import com.waitless.benefit.point.domain.vo.PointType;
 import com.waitless.common.event.ReviewCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +14,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ReviewCreatedEventHandler {
 
-    private final PointRepository pointRepository;
+    private final PointService pointService;
 
     private static final int REWARD_POINT = 100;
 
@@ -25,15 +24,12 @@ public class ReviewCreatedEventHandler {
     public void handleReviewCreatedEvent(ReviewCreatedEvent event) {
         log.info("[Kafka] ReviewCreatedEvent 수신: {}", event);
 
-        // 포인트 생성
-        Point point = Point.of(
+        PostPointCommand command = new PostPointCommand(
                 event.getUserId(),
-                PointAmount.of(REWARD_POINT),
-                PointType.of(PointType.Type.REVIEW_REWARD),
+                100,
+                PointType.Type.REVIEW_REWARD,
                 "[리뷰 작성 보상] 리뷰 ID: " + event.getReviewId()
         );
-
-        pointRepository.save(point);
-        log.info("포인트 적립 완료: userId={}, point={}", point.getUserId(), point.getAmount().getPointValue());
+        pointService.createPoint(command);
     }
 }
