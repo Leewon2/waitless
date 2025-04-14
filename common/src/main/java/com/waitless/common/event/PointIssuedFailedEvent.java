4acf8cecddd2a89c8c98a5881fbf2d7715869039
2sminn <sungmingod13@gmail.com>
2025-04-14 22:45:32 +0900

    [feat]:PointIssuedEvent 리팩토링, PointIssuedFailedEvent 생성 #72

--- common/src/main/java/com/waitless/common/event/PointIssuedFailedEvent.java
+++ common/src/main/java/com/waitless/common/event/PointIssuedFailedEvent.java
@@ -2,18 +2,13 @@ package com.waitless.common.event;
 
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(staticName = "of")
 @Builder
-public class PointIssuedEvent extends Event{
-    private UUID pointId;
+public class PointIssuedFailedEvent extends Event {
     private UUID reviewId;
     private Long userId;
-    private Integer amount;
-    private String description;
-    private LocalDateTime issuedAt;
 }
