3de7b540c7010c93fda7f96cfd6fd916d5dba618
2sminn <sungmingod13@gmail.com>
2025-04-15 14:38:46 +0900

    [feat]:리뷰삭제API를 위한 mapping 추가 #72

--- review-service/src/main/java/com/waitless/review/presentation/mapper/ReviewControllerMapper.java
+++ review-service/src/main/java/com/waitless/review/presentation/mapper/ReviewControllerMapper.java
@@ -1,10 +1,13 @@
 package com.waitless.review.presentation.mapper;
 
+import com.waitless.review.application.dto.command.DeleteReviewCommand;
 import com.waitless.review.application.dto.command.PostReviewCommand;
+import com.waitless.review.presentation.dto.request.DeleteReviewRequestDto;
 import com.waitless.review.presentation.dto.request.PostReviewRequestDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ReviewControllerMapper {
     PostReviewCommand toCommand(PostReviewRequestDto requestDto);
+    DeleteReviewCommand toCommand(DeleteReviewRequestDto requestDto);
 }
