ef26e6101154535b18a9d7ec567d34d1ddd21445
Allrace <jeonij23@gmail.com>
2025-04-15 16:40:33 +0900

    feat: @EnableFeignClients 어노테이션 활성화 #81

--- reservation-service/src/main/java/com/waitless/reservation/ReservationApplication.java
+++ reservation-service/src/main/java/com/waitless/reservation/ReservationApplication.java
@@ -2,10 +2,12 @@ package com.waitless.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = "com.waitless")
 @EnableJpaAuditing
+@EnableFeignClients
 public class ReservationApplication {
 
 	public static void main(String[] args) {
