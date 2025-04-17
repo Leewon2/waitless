bdecc73b7dc8310c66be7f94f9855c1b4e60b887
2sminn <sungmingod13@gmail.com>
2025-04-17 22:52:05 +0900

    [feat]:ControllerMapper에 RequestDto -> SearchCondition 변환 매핑 추가 #96

--- review-service/src/main/java/com/waitless/review/presentation/mapper/ReviewControllerMapper.java
+++ review-service/src/main/java/com/waitless/review/presentation/mapper/ReviewControllerMapper.java
@@ -2,12 +2,21 @@ package com.waitless.review.presentation.mapper;
 
 import com.waitless.review.application.dto.command.DeleteReviewCommand;
 import com.waitless.review.application.dto.command.PostReviewCommand;
-import com.waitless.review.presentation.dto.request.DeleteReviewRequestDto;
-import com.waitless.review.presentation.dto.request.PostReviewRequestDto;
+import com.waitless.review.domain.vo.ReviewSearchCondition;
+import com.waitless.review.presentation.dto.request.*;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ReviewControllerMapper {
     PostReviewCommand toCommand(PostReviewRequestDto requestDto);
     DeleteReviewCommand toCommand(DeleteReviewRequestDto requestDto);
+    default ReviewSearchCondition toCondition(GetReviewRequestDto requestDto) {
+        return new ReviewSearchCondition(requestDto.reviewId(), null, null, null);
+    }
+    default ReviewSearchCondition toCondition(GetReviewListRequestDto requestDto) {
+        return new ReviewSearchCondition(null, requestDto.userId(), requestDto.restaurantId(), null);
+    }
+    default ReviewSearchCondition toCondition(SearchReviewsRequestDto requestDto) {
+        return new ReviewSearchCondition(null, requestDto.userId(), requestDto.restaurantId(), requestDto.rating());
+    }
 }
