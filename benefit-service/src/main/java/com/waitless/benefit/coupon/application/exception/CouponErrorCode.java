a6b4dca8a0663c5c4dab1444054f42bf5865600c
hanjihae <wlgo32@naver.com>
2025-04-17 11:50:40 +0900

    feat : coupon-history 분산락
    - 쿠폰 받기, 쿠폰 조회 Redis 캐싱
    - Redisson 분산락
    
    #110

--- benefit-service/src/main/java/com/waitless/benefit/coupon/application/exception/CouponErrorCode.java
+++ benefit-service/src/main/java/com/waitless/benefit/coupon/application/exception/CouponErrorCode.java
@@ -15,7 +15,8 @@ public enum CouponErrorCode implements ErrorCode {
 	COUPONHISTORY_NOT_FOUND("COUPON_004", "해당 쿠폰발급내역을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 	COUPONHISTORY_UNAUTHORIZED("COUPON_005", "해당 쿠폰발급내역에 대한 권한이 없습니다.", HttpStatus.UNAUTHORIZED),
 	ISSUED_COUPON_EXPIRED("COUPON_006", "해당 쿠폰은 만료되었습니다.", HttpStatus.BAD_REQUEST),
-	COUPON_ALREADY_USED("COUPON_007", "해당 쿠폰은 이미 사용되었습니다.", HttpStatus.BAD_REQUEST);
+	COUPON_ALREADY_USED("COUPON_007", "해당 쿠폰은 이미 사용되었습니다.", HttpStatus.BAD_REQUEST),
+	COUPONHISTORY_TRY_AGAIN("COUPON_008", "다른 사용자가 쿠폰을 발급중입니다. 잠시 후 다시 한번 시도해주세요.", HttpStatus.BAD_REQUEST);
 
 	private final String code;
 	private final String message;
--- benefit-service/src/main/java/com/waitless/benefit/coupon/application/service/CouponHistoryServiceImpl.java
+++ benefit-service/src/main/java/com/waitless/benefit/coupon/application/service/CouponHistoryServiceImpl.java
@@ -1,13 +1,17 @@
 package com.waitless.benefit.coupon.application.service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +21,6 @@ import com.waitless.benefit.coupon.application.dto.ReadCouponHistoriesDto;
 import com.waitless.benefit.coupon.application.exception.CouponBusinessException;
 import com.waitless.benefit.coupon.application.exception.CouponErrorCode;
 import com.waitless.benefit.coupon.application.mapper.CouponHistoryServiceMapper;
-import com.waitless.benefit.coupon.domain.entity.Coupon;
 import com.waitless.benefit.coupon.domain.entity.CouponHistory;
 import com.waitless.benefit.coupon.domain.repository.CouponHistoryRepository;
 import com.waitless.benefit.coupon.infrastructure.repository.CustomCouponHistoryRepository;
@@ -32,33 +35,57 @@ public class CouponHistoryServiceImpl implements CouponHistoryService{
 	private final CustomCouponHistoryRepository customCouponHistoryRepository;
 	private final CouponHistoryServiceMapper couponHistoryServiceMapper;
 	private final CouponService couponService;
+	private final RedisTemplate<String, Object> redisTemplate;
+	private final RedissonClient redissonClient;
 
 	// 쿠폰 받기
 	@Override
 	@Transactional
-	public CouponHistoryResponseDto issuedCoupon(UUID couponId, String userId) { // redisson 분산 락...
-		CouponResponseDto couponInfo = couponService.findCoupon(couponId);
-		// 쿠폰 수량 확인 후
-		if (couponInfo.amount() <= 0) {
-			throw CouponBusinessException.from(CouponErrorCode.COUPON_AMOUNT_EXHAUSTED);
-		}
-		LocalDateTime today = LocalDateTime.now();
-		// 쿠폰 발급 가능일자가 지나면 예외처리
-		if (!today.isBefore(couponInfo.issuanceDate())) {
+	public CouponHistoryResponseDto issuedCoupon(UUID couponId, Long userId) {
+		String lockKey = "LOCK:CH:" + couponId;
+		RLock lock = redissonClient.getLock(lockKey);
+		boolean isLocked = false;
+		try {
+			isLocked = lock.tryLock(5, 3, TimeUnit.SECONDS);
+			if (!isLocked) {
+				throw CouponBusinessException.from(CouponErrorCode.COUPONHISTORY_TRY_AGAIN);
+			}
+
+			// 쿠폰 발급 시작
+			CouponResponseDto couponInfo = couponService.findCoupon(couponId);
+			// 쿠폰 수량 확인 후
+			if (couponInfo.amount() <= 0) {
+				throw CouponBusinessException.from(CouponErrorCode.COUPON_AMOUNT_EXHAUSTED);
+			}
+			LocalDateTime today = LocalDateTime.now();
+			// 쿠폰 발급 가능일자가 지나면 예외처리
+			if (!today.isBefore(couponInfo.issuanceDate())) {
+				throw CouponBusinessException.from(CouponErrorCode.COUPON_ISSUED_IMPOSSIBLE);
+			}
+			// 쿠폰 사용 가능 일자
+			LocalDateTime expiredDate = today.plusDays(couponInfo.validPeriod());
+			CouponHistory couponHistory = CouponHistory.builder()
+				.title(couponInfo.title())
+				.couponId(couponId)
+				.userId(userId)
+				.isValid(true)
+				.expiredAt(expiredDate)
+				.build();
+			// 쿠폰 발급가능수량 -1 차감
+			couponService.decreaseCouponAmount(couponId);
+			CouponHistory saved = couponHistoryRepository.save(couponHistory);
+			// Redis 캐싱
+			redisTemplate.opsForValue().set("CH:"+ saved.getId(), couponHistory, Duration.ofHours(1));
+
+			return couponHistoryServiceMapper.toCouponHistoryResponseDto(saved);
+		} catch(InterruptedException e) {
+			Thread.currentThread().interrupt();
 			throw CouponBusinessException.from(CouponErrorCode.COUPON_ISSUED_IMPOSSIBLE);
+		} finally {
+			if (isLocked && lock.isHeldByCurrentThread()) {
+				lock.unlock();
+			}
 		}
-		// 쿠폰 사용 가능 일자
-		LocalDateTime expiredDate = today.plusDays(couponInfo.validPeriod());
-		CouponHistory couponHistory = CouponHistory.builder()
-			.title(couponInfo.title())
-			.couponId(couponId)
-			.userId(Long.parseLong(userId))
-			.isValid(true)
-			.expiredAt(expiredDate)
-			.build();
-		// 쿠폰 발급가능수량 -1 차감
-		couponService.decreaseCouponAmount(couponId);
-		return couponHistoryServiceMapper.toCouponHistoryResponseDto(couponHistoryRepository.save(couponHistory));
 	}
 
 	// 쿠폰발급내역 단건 조회
@@ -88,9 +115,9 @@ public class CouponHistoryServiceImpl implements CouponHistoryService{
 	// 쿠폰발급내역 삭제
 	@Override
 	@Transactional
-	public void removeCouponHistory(UUID id, String userId) {
+	public void removeCouponHistory(UUID id, Long userId) {
 		CouponHistory couponHistory = findCouponHistoryById(id);
-		if (!couponHistory.getUserId().equals(Long.parseLong(userId))) {
+		if (!couponHistory.getUserId().equals(userId)) {
 			throw CouponBusinessException.from(CouponErrorCode.COUPONHISTORY_UNAUTHORIZED);
 		}
 		couponHistory.delete();
@@ -99,10 +126,9 @@ public class CouponHistoryServiceImpl implements CouponHistoryService{
 	// 발급된 쿠폰 사용
 	@Override
 	@Transactional
-	public void userIssuedCoupon(UUID id, String userId) {
+	public void userIssuedCoupon(UUID id, Long userId) {
 		CouponHistory couponHistory = findCouponHistoryById(id);
-		long loginuser = Long.parseLong(userId);
-		if (!couponHistory.getUserId().equals(loginuser)) {
+		if (!couponHistory.getUserId().equals(userId)) {
 			throw CouponBusinessException.from(CouponErrorCode.COUPONHISTORY_UNAUTHORIZED);
 		}
 		if (couponHistory.getExpiredAt().isBefore(LocalDateTime.now())) {
@@ -115,8 +141,15 @@ public class CouponHistoryServiceImpl implements CouponHistoryService{
 	}
 
 	private CouponHistory findCouponHistoryById(UUID id) {
+		String key = "CH:" + id;
+		CouponHistory cached = (CouponHistory) redisTemplate.opsForValue().get(key);
+		if (cached != null) {
+			return cached;
+		}
 		CouponHistory couponHistory = couponHistoryRepository.findById(id)
 			.orElseThrow(()-> CouponBusinessException.from(CouponErrorCode.COUPONHISTORY_NOT_FOUND));
+		redisTemplate.opsForValue().set(key, couponHistory, Duration.ofHours(1));
+
 		return couponHistory;
 	}
 
