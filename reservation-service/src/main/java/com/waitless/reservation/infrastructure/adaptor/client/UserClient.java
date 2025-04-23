97622521d2d23fa2dfe59c7b8671561a5f3f75e7
Allrace <jeonij23@gmail.com>
2025-04-23 21:16:47 +0900

    fix: FeingClient 응답 예외처리 수정 #133

--- reservation-service/src/main/java/com/waitless/reservation/infrastructure/adaptor/client/UserClient.java
+++ reservation-service/src/main/java/com/waitless/reservation/infrastructure/adaptor/client/UserClient.java
@@ -2,6 +2,7 @@ package com.waitless.reservation.infrastructure.adaptor.client;
 
 import com.waitless.common.exception.response.SingleResponse;
 import com.waitless.reservation.infrastructure.adaptor.client.dto.UserResponseDto;
+import com.waitless.reservation.infrastructure.config.feign.FeignCommonConfig;
 import com.waitless.reservation.infrastructure.config.feign.UserFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(
         name = "user-service",
         url = "${gateway.url}",
-        configuration = UserFeignConfig.class
+        configuration = {FeignCommonConfig.class, UserFeignConfig.class}
 )
 public interface UserClient {
     @GetMapping("/user-service/api/users/{userId}")
