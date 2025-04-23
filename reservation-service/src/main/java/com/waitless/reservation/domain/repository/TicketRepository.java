65b0ced6b1e1c9af9dd07d19180b4f25d3d1ebf4
Allrace <jeonij23@gmail.com>
2025-04-23 22:07:30 +0900

    feat: http-test 수정 및 스케쥴러 강제 호출 로직 구현

--- reservation-service/src/main/java/com/waitless/reservation/domain/repository/TicketRepository.java
+++ reservation-service/src/main/java/com/waitless/reservation/domain/repository/TicketRepository.java
@@ -4,9 +4,12 @@ import com.waitless.reservation.domain.entity.TicketRestaurant;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.UUID;
 
 public interface TicketRepository {
     void save(TicketRestaurant ticketRestaurant);
 
     List<TicketRestaurant> findByOpenTime(LocalTime openTime);
+
+    List<TicketRestaurant> findByRestaurantId(UUID restaurantId);
 }
