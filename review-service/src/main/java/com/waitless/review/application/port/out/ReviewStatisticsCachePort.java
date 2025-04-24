29556d0a3944a14b0bf6c4d2a363fe385e0d2793
2sminn <sungmingod13@gmail.com>
2025-04-24 15:58:00 +0900

    [feat]: Redis mget 방식 추가 + pipelined 기능 추가(RedisCallback처리) #158

--- review-service/src/main/java/com/waitless/review/application/port/out/ReviewStatisticsCachePort.java
+++ review-service/src/main/java/com/waitless/review/application/port/out/ReviewStatisticsCachePort.java
@@ -2,8 +2,11 @@ package com.waitless.review.application.port.out;
 
 import com.waitless.review.domain.vo.ReviewStatisticsCache;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ReviewStatisticsCachePort {
-    void save(String restaurantId, ReviewStatisticsCache data, long ttlSeconds);
-    ReviewStatisticsCache find(String restaurantId);
+    void saveBatch(Map<String, ReviewStatisticsCache> data, long ttlSeconds);
+    Map<String, ReviewStatisticsCache> findBatch(List<String> restaurantIds);
     void delete(String restaurantId);
 }
