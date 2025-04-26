70dc98e67c0210f4d741933b3ad7ac98fb04d773
hanjihae <wlgo32@naver.com>
2025-04-27 02:14:40 +0900

    feat : auth, user - RoleCheck 추가
    - 필요한 API에 RoleCheck 추가
    - Role.ALL 없어도 될 듯
    
    #161

--- user-service/src/main/java/com/waitless/user/presentation/controller/UserController.java
+++ user-service/src/main/java/com/waitless/user/presentation/controller/UserController.java
@@ -13,10 +13,13 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.waitless.common.aop.RoleCheck;
+import com.waitless.common.domain.Role;
 import com.waitless.common.exception.response.MultiResponse;
 import com.waitless.common.exception.response.SingleResponse;
 import com.waitless.user.application.dto.SignupResponseDto;
@@ -52,19 +55,23 @@ public class UserController {
 
 	// 유저 단건 조회
 	@GetMapping("/{id}")
-	public ResponseEntity<SingleResponse<UserResponseDto>> readUser(@PathVariable Long id) {
-		return ResponseEntity.ok(SingleResponse.success(userService.findUser(id)));
+	public ResponseEntity<SingleResponse<UserResponseDto>> readUser(@PathVariable Long id, @RequestHeader("X-User-Role") String roleHeader) {
+		Role role = Role.valueOf(roleHeader.toUpperCase());
+		return ResponseEntity.ok(SingleResponse.success(userService.findUser(id, role)));
 	}
 
 	// 유저 전체 조회
+	@RoleCheck(roles = {Role.ADMIN, Role.OWNER})
 	@GetMapping
 	public ResponseEntity<?> readUsers(
 		@RequestParam(required = false) String name,
 		@RequestParam(defaultValue = "1", required = false) int page,
 		@RequestParam(defaultValue = "10", required = false) int size,
 		@RequestParam(defaultValue = "DESC", required = false) Sort.Direction sortDirection,
-		@RequestParam(defaultValue = "updatedAt", required = false) String sortBy
+		@RequestParam(defaultValue = "updatedAt", required = false) String sortBy,
+		@RequestHeader("X-User-Role") String role
 	) {
+		System.out.println(role);
 		ReadUsersRequestDto readUsersRequestDto = new ReadUsersRequestDto(name, page, size, sortDirection, sortBy);
 		Pageable pageable = PageRequest.of(page - 1, size);
 		Page<UserResponseDto> response = userService.findAndSearchUsers(userControllerMapper.toReadUsersDto(readUsersRequestDto), pageable);
@@ -73,15 +80,20 @@ public class UserController {
 
 	// 유저 수정
 	@PatchMapping("/{id}")
-	public ResponseEntity<SingleResponse<UserResponseDto>> updateUser(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
-		UserResponseDto userResponseDto = userService.modifyUser(id, updates);
+	public ResponseEntity<SingleResponse<UserResponseDto>> updateUser(
+		@PathVariable Long id,
+		@RequestBody Map<String, Object> updates,
+		@RequestHeader("X-User-Role") String roleHeader) {
+		Role role = Role.valueOf(roleHeader.toUpperCase());
+		UserResponseDto userResponseDto = userService.modifyUser(id, updates, role);
 		return ResponseEntity.ok(SingleResponse.success(userResponseDto));
 	}
 
 	// 유저 삭제
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-		userService.removeUser(id);
+	public ResponseEntity<Void> deleteUser(@PathVariable Long id, @RequestHeader("X-User-Role") String roleHeader) {
+		Role role = Role.valueOf(roleHeader.toUpperCase());
+		userService.removeUser(id, role);
 		return ResponseEntity.noContent().build();
 	}
 }
