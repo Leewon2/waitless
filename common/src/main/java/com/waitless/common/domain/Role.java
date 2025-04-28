789f6168795ee7698bba392db65401728ecc3749
hanjihae <wlgo32@naver.com>
2025-04-28 14:23:53 +0900

    feat : common - Role.ALL 삭제
    - CouponController에 있는 것까지 삭제
    
    #161

--- common/src/main/java/com/waitless/common/domain/Role.java
+++ common/src/main/java/com/waitless/common/domain/Role.java
@@ -6,6 +6,5 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-	ADMIN, USER, OWNER,
-	@SuppressWarnings("unused") ALL;	// 어노테이션에서만 사용, 요청 헤더에는 안 들어옴.
+	ADMIN, USER, OWNER
 }
