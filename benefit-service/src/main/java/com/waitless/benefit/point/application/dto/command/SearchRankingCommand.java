d18a7fd866a28b2ede271a5453a63f62d31f1a6d
2sminn <sungmingod13@gmail.com>
2025-04-30 01:08:07 +0900

    [feat]: Command에 UserInfoDto 추가 #176

--- benefit-service/src/main/java/com/waitless/benefit/point/application/dto/command/SearchRankingCommand.java
+++ benefit-service/src/main/java/com/waitless/benefit/point/application/dto/command/SearchRankingCommand.java
@@ -1,5 +1,8 @@
 package com.waitless.benefit.point.application.dto.command;
 
+import com.waitless.common.domain.UserInfoDto;
+
 public record SearchRankingCommand(
-        int topN
+        int topN,
+        UserInfoDto userInfoDto
 ) {}
