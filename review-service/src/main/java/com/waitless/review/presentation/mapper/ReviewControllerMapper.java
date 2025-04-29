69070630d6defb8a6a74a20992a685ea35443ba9
2sminn <sungmingod13@gmail.com>
2025-04-30 01:05:09 +0900

    [feat]: ControllerMapper에 @Mapping추가 및 UserInfoDto추가 #176

--- review-service/src/main/java/com/waitless/review/presentation/mapper/ReviewControllerMapper.java
+++ review-service/src/main/java/com/waitless/review/presentation/mapper/ReviewControllerMapper.java
@@ -1,16 +1,29 @@
 package com.waitless.review.presentation.mapper;
 
+import com.waitless.common.domain.UserInfoDto;
 import com.waitless.review.application.dto.command.DeleteReviewCommand;
 import com.waitless.review.application.dto.command.PostReviewCommand;
 import com.waitless.review.application.dto.command.ReviewStatisticsCommand;
 import com.waitless.review.domain.vo.ReviewSearchCondition;
 import com.waitless.review.presentation.dto.request.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReviewControllerMapper {
-    PostReviewCommand toCommand(PostReviewRequestDto requestDto);
-    DeleteReviewCommand toCommand(DeleteReviewRequestDto requestDto);
+
+    @Mapping(source = "requestDto.reservationId", target = "reservationId")
+    @Mapping(source = "requestDto.restaurantId", target = "restaurantId")
+    @Mapping(source = "requestDto.content", target = "content")
+    @Mapping(source = "requestDto.rating", target = "rating")
+    @Mapping(source = "userInfoDto.userId", target = "userId")
+    @Mapping(source = "userInfoDto", target = "userInfoDto")
+    PostReviewCommand toCommand(PostReviewRequestDto requestDto, UserInfoDto userInfoDto);
+
+    @Mapping(source = "requestDto.reviewId", target = "reviewId")
+    @Mapping(source = "userInfoDto.userId", target = "userId")
+    @Mapping(source = "userInfoDto", target = "userInfoDto")
+    DeleteReviewCommand toCommand(DeleteReviewRequestDto requestDto, UserInfoDto userInfoDto);
     default ReviewSearchCondition toCondition(GetReviewRequestDto requestDto) {
         return new ReviewSearchCondition(requestDto.reviewId(), null, null, null);
     }
@@ -20,7 +33,5 @@ public interface ReviewControllerMapper {
     default ReviewSearchCondition toCondition(SearchReviewsRequestDto requestDto) {
         return new ReviewSearchCondition(null, requestDto.userId(), requestDto.restaurantId(), requestDto.rating());
     }
-    default ReviewStatisticsCommand toCommand(ReviewStatisticsBatchRequestDto requestDto) {
-        return new ReviewStatisticsCommand(requestDto.restaurantIds());
-    }
+    ReviewStatisticsCommand toCommand(ReviewStatisticsBatchRequestDto requestDto, UserInfoDto userInfoDto);
 }
